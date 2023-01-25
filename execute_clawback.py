f0799fd011ff91ffc33c61f3110ab0e0e9c7bd48
vmalepati1 <malepativikas@gmail.com>
2023-01-24 21:27:04 -0500

    Enable clawback on asset

---
+++ execute_clawback.py
@@ -0,0 +1,60 @@
+from stellar_sdk import Asset, Keypair, Network, Server, TransactionBuilder, SetOptions
+from stellar_sdk import AuthorizationFlag
+
+# Configure Stellar SDK to talk to the horizon instance hosted by Stellar.org
+# To use the live network, set the hostname to 'https://horizon.stellar.org'
+server = Server(horizon_url="https://horizon-testnet.stellar.org")
+# Use test network, if you need to use public network, please set it to `Network.PUBLIC_NETWORK_PASSPHRASE`
+network_passphrase = Network.TESTNET_NETWORK_PASSPHRASE
+
+# Keys for accounts to issue and receive the new asset
+issuing_keypair = Keypair.from_secret(
+    "SCBDNKUBCBMBPZUGQ4LGBEXLRF5WSPHH7ZI2OJMBN2CLIAKGJT6OCD73"
+)
+issuing_public = issuing_keypair.public_key
+
+distributor_keypair = Keypair.from_secret(
+    "SADWIQXRSDP4GRF7REQ2KNNZD64PATUAIYANLYGGO6YQ6IWKQP3KT57C"
+)
+distributor_public = distributor_keypair.public_key
+
+# Transactions require a valid sequence number that is specific to this account.
+# We can fetch the current sequence number for the source account from Horizon.
+issuing_account = server.load_account(issuing_public)
+distributor_account = server.load_account(distributor_public)
+
+# Create an object to represent the new asset
+some_stock = Asset("SomeStock", issuing_public)
+
+enable_clawback_transaction = (
+    TransactionBuilder(
+        source_account=issuing_account,
+        network_passphrase=network_passphrase,
+        base_fee=100,
+    )
+    .append_operation(SetOptions(set_flags=AuthorizationFlag.AUTHORIZATION_CLAWBACK_ENABLED | AuthorizationFlag.AUTHORIZATION_REVOCABLE))
+    .set_timeout(100)
+    .build()
+)
+
+enable_clawback_transaction.sign(issuing_keypair)
+clawback_transaction_resp = server.submit_transaction(enable_clawback_transaction)
+print(f"Clawback Transaction Resp:\n{clawback_transaction_resp}")
+
+# First, the receiving account must trust the asset
+trust_transaction = (
+    TransactionBuilder(
+        source_account=distributor_account,
+        network_passphrase=network_passphrase,
+        base_fee=100,
+    )
+    #  The `changeTrust` operation creates (or alters) a trustline
+    #  The `limit` parameter below is optional
+    .append_change_trust_op(asset=some_stock, limit="1000")
+    .set_timeout(100)
+    .build()
+)
+
+trust_transaction.sign(distributor_keypair)
+trust_transaction_resp = server.submit_transaction(trust_transaction)
+print(f"Change Trust Transaction Resp:\n{trust_transaction_resp}")
